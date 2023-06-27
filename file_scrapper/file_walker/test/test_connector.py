709b26a63d242ee2f88b11e73fd366c852e293b1
suriya-py-dev <114597902+suriya-py-dev@users.noreply.github.com>
2023-06-27 14:33:22 +0530

    Update test_connector.py

--- file_scrapper/file_walker/test/test_connector.py
+++ file_scrapper/file_walker/test/test_connector.py
@@ -1,4 +1,4 @@
-from file_scrapper.file_walker.connector import Connector
+from file_scrapper.file_walker.connector import GCSConnector, AWSConnector
 
 
 class TestConnector:
@@ -9,18 +9,14 @@ class TestConnector:
     def test_collect_to_aws_client(self, mocker):
         """Test AWS is connected to client"""
         mocked_s3_client = mocker.patch('boto3.client')
-        cntr = Connector()
-        assert cntr.aws_client is None
-        cntr.connect_to_aws_client()
-        assert cntr.aws_client is not None
+        aws_client = AWSConnector().connect_to_client()
+        assert aws_client is not None
 
     def test_collect_to_gcs_client(self, mocker):
         """Test GCS is connected to its client"""
         mocked_gcs_client = mocker.patch('google.cloud.storage.Client')
-        cntr = Connector()
-        assert cntr.gcs_client is None
-        cntr.connect_to_gcs_client()
-        assert cntr.gcs_client is not None
+        gcs_client = GCSConnector().connect_to_client()
+        assert gcs_client is not None
 
     def test_upload_to_gcs_resource(self, tmpdir, mocker):
         """Test the GCS client is uploaded the file to cloud"""
@@ -29,9 +25,9 @@ class TestConnector:
         image_file1 = image_dir.join('image1.jpg')
         image_file1.write('Content of image1.jpg')
         mocked_gcs_client = mocker.patch('google.cloud.storage.Client')
-        cntr = Connector()
-        cntr.connect_to_gcs_client()
-        cntr.upload_to_gcs_resource(image_file1.dirname, "GCS-BUCKET")
+        cntr = GCSConnector()
+        client = cntr.connect_to_client()
+        cntr.upload_to_resource(image_file1.dirname, "GCS-BUCKET", "client", client)
         mocked_gcs_client.assert_called_once()
 
     def test_upload_to_aws_resource(self, tmpdir, mocker):
@@ -41,7 +37,7 @@ class TestConnector:
         media_file1 = media_dir.join('media1.mp4')
         media_file1.write('Content of media1.mp4')
         mocked_aws_client = mocker.patch('boto3.client')
-        cntr = Connector()
-        cntr.connect_to_aws_client()
-        cntr.upload_to_aws_resource(media_file1.dirname, "AWS-BUCKET")
+        cntr = AWSConnector()
+        client = cntr.connect_to_client()
+        cntr.upload_to_resource(media_file1.dirname, "AWS-BUCKET", "client", client)
         mocked_aws_client.assert_called_once()
